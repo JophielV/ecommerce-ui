8482fe1394cc4c19e94abbf0e9373b0665888bbc
JophielV <jophielvillanueva@gmail.com>
2021-10-10 01:00:33 +0800

    Shared UI initial setup

---
+++ src/app/shared/models/ng-bootstrap.module.ts
@@ -0,0 +1,54 @@
+import { NgModule } from '@angular/core';
+import {
+  NgbAlertModule,
+  NgbButtonsModule,
+  NgbDatepickerModule,
+  NgbDropdownModule,
+  NgbModalModule,
+  NgbTooltipModule,
+  NgbPopoverModule,
+  NgbProgressbarModule,
+  NgbTypeaheadModule
+} from '@ng-bootstrap/ng-bootstrap';
+
+// uncomment as needed
+@NgModule({
+  imports: [
+    // NgbAccordion,
+    NgbAlertModule,
+    NgbButtonsModule,
+    // NgbCarouselModule,
+    // NgbCollapseModule,
+    NgbDatepickerModule,
+    NgbDropdownModule,
+    NgbModalModule,
+    // NgbPaginationModule,
+    NgbPopoverModule,
+    NgbProgressbarModule,
+    // NgbRatingModule,
+    // NgbTabsetModule,
+    // NgbTimepickerModule,
+    NgbTooltipModule,
+    NgbTypeaheadModule
+  ],
+  exports: [
+    // NgbAccordion,
+    NgbAlertModule,
+    NgbButtonsModule,
+    // NgbCarouselModule,
+    // NgbCollapseModule,
+    NgbDatepickerModule,
+    NgbDropdownModule,
+    NgbModalModule,
+    // NgbPaginationModule,
+    NgbPopoverModule,
+    NgbProgressbarModule,
+    // NgbRatingModule,
+    // NgbTabsetModule,
+    // NgbTimepickerModule,
+    NgbTooltipModule,
+    NgbTypeaheadModule
+  ]
+})
+export class NgBootstrapModule {}
+
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,6 +7,12 @@ import {ChangePasswordComponent} from '../core/modules/auth/change-password/chan
 import {NgxUpperCaseDirectiveModule} from 'ngx-upper-case-directive';
 import {ShowHidePasswordModule} from 'ngx-show-hide-password';
 import {TopBarComponent} from './components/layout/top-bar/top-bar.component';
+import {NgSelectModule} from '@ng-select/ng-select';
+import {NzUploadModule} from 'ng-zorro-antd';
+import {NgxDatatableModule} from '@swimlane/ngx-datatable';
+import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
+import {BsDatepickerModule} from 'ngx-bootstrap/datepicker';
+import {NgBootstrapModule} from './models/ng-bootstrap.module';
 
 
 
@@ -17,8 +23,14 @@ import {TopBarComponent} from './components/layout/top-bar/top-bar.component';
   ],
   imports: [
     CommonModule,
+    BsDatepickerModule.forRoot(),
+    BsDropdownModule.forRoot(),
     FormsModule,
     ReactiveFormsModule,
+    NgSelectModule,
+    NgBootstrapModule,
+    NgxDatatableModule,
+    NzUploadModule,
     NgxUpperCaseDirectiveModule,
     RouterModule,
     ShowHidePasswordModule
